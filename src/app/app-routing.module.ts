d6fa333989b2e60e709fac4993af8e41f7e6cc65
jessica-siles <silesjessica@hotmail.com>
2021-06-16 18:17:43 -0300

    2 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,12 @@ import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import {EmpresasComponent } from './empresas/empresas.component';
-
+import { EmpresasComponent } from './empresas/empresas.component';
+import { PruebaComponent } from './prueba/prueba.component';
 
 export const router: Routes = [
-
-    { path: 'empresas',  component: EmpresasComponent }
-    
+  { path: 'empresas', component: EmpresasComponent },
+  { path: 'prueba', component: PruebaComponent }
 ];
 
 export const routes: ModuleWithProviders = RouterModule.forRoot(router);
